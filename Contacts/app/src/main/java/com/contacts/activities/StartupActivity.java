09ff4b672b302710e74abe0904a443fdc37ae771
jhiertz <jordan.hiertz@al-enterprise.com>
2019-09-11 14:34:53 +0200

    Update to androidx

--- Contacts/app/src/main/java/com/contacts/activities/StartupActivity.java
+++ Contacts/app/src/main/java/com/contacts/activities/StartupActivity.java
@@ -7,14 +7,15 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.ale.infra.contact.IRainbowContact;
 import com.ale.listener.SignoutResponseListener;
--- Contacts/app/src/main/java/com/contacts/fragments/LoginFragment.java
+++ Contacts/app/src/main/java/com/contacts/fragments/LoginFragment.java
@@ -5,13 +5,14 @@ import android.app.Fragment;
 import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 
 import com.ale.listener.SigninResponseListener;
 import com.ale.listener.StartResponseListener;
